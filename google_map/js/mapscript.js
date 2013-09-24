a992a8343453ebf4b0765bb6ceed94b6d82b25f1
Jim Kollevik <jim.kollevik@gmail.com>
2013-09-24 14:58:26 +0200

    updated script with comments

--- google_map/js/mapscript.js
+++ google_map/js/mapscript.js
@@ -1,5 +1,5 @@
 function initialize() {
-
+        //here is the starting for the map, where it will begin to show
         var latlng = new google.maps.LatLng(61.773123,14.751892);
 
         var myOptions = {
@@ -7,13 +7,13 @@ function initialize() {
           center: latlng,
           mapTypeId: google.maps.MapTypeId.ROADMAP
         };
-
+          //below are the markers coordinates, change it to your coordinates
         var map = new google.maps.Map(document.getElementById('map_canvas'), myOptions);
           var trucks = [
           [59.333682, 18.015408],
           [59.298544, 17.996718]
           ];
-
+          //script counts the array of coordinates
           for (var i = 0; i < trucks.length; i++) {
             var marker = new google.maps.Marker({
               position: new google.maps.LatLng(trucks[i][0], trucks[i][1]),
