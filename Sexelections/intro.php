6485ab85e8b479da62807d73957b47b68ab28a2b
Jim Kollevik <jim.kollevik@gmail.com>
2014-04-05 13:13:58 +0200

    intro page

---
+++ Sexelections/intro.php
@@ -0,0 +1,121 @@
+<?php 
+
+	include 'header.php';
+
+?>
+
+<style>
+
+html {
+  background-color: #000000;
+}
+	
+body {
+	background:  url(img/landingpage.jpg);
+	background-size: 1320px;
+	background-repeat: no-repeat;
+	background-position: center;
+	background-size: cover;
+	min-width: 1200px;
+	display: none;
+}
+
+#headline1 {
+color: #E8E8E8;
+font-family: "Duru Sans", sans-serif;
+font-size: 77px;
+width: 900px;
+margin: 0;
+margin-left: -60%;
+margin-top: 200px;
+}
+
+#headline2 {
+	margin-left: -7% !important;
+}
+
+#wrapper {
+	background-color: #FFFFFF;
+	height: 65%;
+	width: 70%;
+	margin: 120px 0 0 15%;
+	position: absolute;
+	opacity: 0.9;
+}
+
+h1 {
+	font-family: 'whitney_htfbook';
+	font-size: 2em;
+}
+
+h2 {
+	font-family: 'whitney_htfbook';
+	font-size: 1.8em;
+}
+
+#sec_par {
+	margin-top: 60px;
+	font-family: index-headline;
+}
+
+h1 {
+	margin: 30px 30px 30px 30px;
+	text-align: center;
+}
+
+h2 {
+	margin: -10px 30px 30px 30px;
+}
+
+p {
+	font-family: index-headline;
+	margin: 20px 30px 30px 30px;
+	font-size: 1.5em;
+	text-align: center;
+}
+
+#back {
+  width: 70px;
+  height: 35px;
+  margin-left: 30px;
+  cursor: pointer;
+  margin: 0 auto;
+}
+
+#back a {
+  text-decoration: none;
+  font-size: 20px;
+  color: red;
+  font-family: next-button;
+  letter-spacing: 1px;
+}
+
+#back a:hover{
+  color: #000000;
+      transition:0.3s;  
+}
+
+</style>
+
+</head>
+<body>
+
+	<div id="wrapper">
+
+		<h1>Hello sexy ones! We are the Data Dynasty group:</h1>
+		<p>
+Jo Posselt<br>
+Jim Kollevik<br>
+Vanja Folin<br>
+Diego Fria<br>
+:)
+  <p>
+
+		<div id="back">
+			<a href="http://www.sexelections.se">START!</a>
+		</div><!-- end back -->
+
+	</div><!-- end wrapper -->
+
+</body>
+</html>
