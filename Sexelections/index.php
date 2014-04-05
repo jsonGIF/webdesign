32cf6ab209187420d45945e76e1b44039aaf4209
Jim Kollevik <jim.kollevik@gmail.com>
2014-04-05 13:04:54 +0200

    index file

---
+++ Sexelections/index.php
@@ -0,0 +1,67 @@
+<?php 
+
+	include 'header.php';
+
+?>
+
+<style>
+
+html {
+  background-color: #000000;
+}
+	
+body {
+	background:  url(img/landingpage.jpg);
+	background-size: 1320px;
+	background-repeat: no-repeat;
+	background-position: center;
+	background-size: cover;
+	min-width: 1200px;
+	display: none;
+}
+
+#headline1 {
+color: #E8E8E8;
+font-family: "Duru Sans", sans-serif;
+font-size: 77px;
+width: 900px;
+margin: 0;
+margin-left: -60%;
+}
+
+</style>
+
+</head>
+<body>
+
+<div id="headline">
+	<h1 id="headline1">THE SEX ELECTIONS</h1>
+	<h2 id="headline2">which one is the most potent party?</h2>
+</div>
+
+
+
+<div id="text">
+	<p>Ah Swedish politics. Left or right. Red or blue, Social Democrats or Moderates. Elections are coming up this year but we can't place our votes without knowing the who's into what, where and whom. 
+ </p>
+
+</div>
+
+<div id="btn">
+<a href="q1.php" class="transition"><h2>GET IT ON!</h2></a>
+</div>
+
+	<div id="slogo">
+
+<img class="logo" src="img/slogo.png">
+
+</div>
+		
+<div id="mlogo">
+
+<img class="logo" src="img/mlogo.png">
+
+</div>
+
+</body>
+</html>
